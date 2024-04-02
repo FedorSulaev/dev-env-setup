9ccb11ded4c3a3912c835dc0de4a793f7494ae46
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-04-02 20:59:37 +0300

    feat: Powerlevel10k setup

--- src/setup.sh
+++ src/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 cd $HOME
-#Oh my zsh setup
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+# Oh my zsh setup
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Powerlevel 10k setup
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed 's|robbyrussell|powerlevel10k/powerlevel10k|' -i ~/.zshrc
+# Setup complete
+zsh
