19fccb3680bc181312faeffd297ac8212a6df5d8
Fedor Sulaev <fedor.sulaev@outlook.com>
2024-04-03 18:56:03 +0300

    feat: Syntax highlighting

--- docker/al2/prerequisites.sh
+++ docker/al2/prerequisites.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum update -y
-yum install -y zsh util-linux-user git
+yum install -y zsh util-linux-user git tar
 chsh -s /usr/bin/zsh $USER
--- src/setup.sh
+++ src/setup.sh
@@ -5,5 +5,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Powerlevel 10k setup
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed 's|robbyrussell|powerlevel10k/powerlevel10k|' -i ~/.zshrc
+# Zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed 's|plugins=(git)|plugins=(git zsh-syntax-highlighting)|' -i ~/.zshrc
 # Setup complete
 zsh
